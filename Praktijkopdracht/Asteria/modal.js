7e0829ed488e95da10808f5dea417e088d0e3ecb
David <David_dekker@live.nl>
2021-11-22 10:08:00 +0100

    praktijk

---
+++ Praktijkopdracht/Asteria/modal.js
@@ -0,0 +1,22 @@
+var modal = document.querySelector("#myModal");
+
+var link = document.querySelectorAll(".link");
+for (var i = 0; i < link.length; i++) {
+    var linkClick= link[i];
+    linkClick.onclick = function () {
+      modal.style.display = "block";
+    };
+}
+
+var span = document.querySelectorAll(".close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+  modframe.location.href='about: blank';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modframe.location.href='about: blank';
+  }
+}
